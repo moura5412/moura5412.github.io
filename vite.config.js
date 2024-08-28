5d03432053fb71377c2c955219a78d558b07ff52
Gabriel Souza <moura5412@protonmail.com>
2024-08-28 15:18:20 -0300

    alt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://moura5412.github.io/',
   build: {
     outDir: 'dist',
     assetsDir: ''
